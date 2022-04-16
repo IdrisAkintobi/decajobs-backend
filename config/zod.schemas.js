e5aac2d8d565537a906f60e61fbe526a46321560
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-04-16 09:07:18 +0100

    Created zod schemas for validation

---
+++ config/zod.schemas.js
@@ -0,0 +1,61 @@
+import { z } from "zod";
+const JobZodSchema = z.object({
+  category: z.string(),
+  closeDate: z.string(),
+  companyName: z.string().min(2, { message: "Enter full company name" }),
+  description: z.string().min(30, {
+    message: "Job description cannot be shorter than 30 characters",
+  }),
+  email: z.string().email({ message: "Invalid email address" }),
+  location: z.string(),
+  salary: z.string(),
+  experience: z.string(),
+  title: z.string({ message: "Enter job title" }),
+  type: z.string(),
+  website: z.string().url(),
+});
+
+const EduZodSchema = z.object({
+  schoolName: z.string(),
+  qualification: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
+});
+
+const ExpZodSchema = z.object({
+  employer: z.string(),
+  position: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
+  description: z.string(),
+});
+
+const ResumeZOdSchema = z.object({
+  FirstName: z.string({ message: "Enter first name" }),
+  LastName: z.string({ message: "Enter last name" }),
+  dob: z.string({ message: "Enter date of birth" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  gender: z.string(),
+  location: z.string(),
+  objective: z.string({ message: "Write your resume objective" }),
+  phone: z.string(),
+  title: z.string({ message: "Enter preferred tittle" }),
+  website: z.string().url(),
+  skills: z.string().array(),
+  experience: ExpZodSchema.optional(),
+  education: EduZodSchema,
+});
+
+const UserSchema = z.object({
+  role: z.enum(["Recruiter", "Applicant"]),
+  fullName: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export { JobZodSchema, ResumeZOdSchema, UserSchema, LoginSchema };
